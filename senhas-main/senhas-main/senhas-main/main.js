4b2e2c2d67c7552ebde329326a6495f87f289795
cls4nt <142804556+cls4nt@users.noreply.github.com>
2025-08-01 11:43:35 -0300

    ok

--- senhas-main/senhas-main/senhas-main/main.js
+++ senhas-main/senhas-main/senhas-main/main.js
@@ -30,3 +30,26 @@ function aumentaTamanho() {
     numeroSenha.textContent = tamanhoSenha;
     gerarSenha();
 }
+
+function gerarSenha() {
+    let alfabeto = '';
+    if (checkbox[0].checked) alfabeto += letrasMaiuscula;
+    if (checkbox[1].checked) alfabeto += letrasMaiuscula;
+    if (checkbox[2].checked) alfabeto += numeros;
+    if (checkbox[3].checked) alfabeto += simbolos;
+
+    if (alfabeto.length === 0) {
+        campoSenha.value = '';
+        classificaSenha(1);
+        return;
+    }
+
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha; i++); {
+    const nuremoAleatorio = Math.floor(Math.random() * alfabeto.length);
+    senha += alfabeto[numeroAleatorio];
+}
+
+campoSenha.value = senha;
+classificaSenha(alfabeto.length)
+}
